1ed947a671438292a1d0766761a6aff62ff84039
Wychbane <davemurphy81@hotmail.com>
2022-08-05 13:36:57 +0100

    Create grid for etch-a-sketch

--- js/js.js
+++ js/js.js
@@ -1,9 +1,22 @@
-
 const container = document.querySelector('.container');
+let cells = document.querySelectorAll('.cell')
 
-for (let i=0; i < 16; i++) {
-    for(let j=0; j < 16; j++) {
-       let div = document.createElement('div');
-       container.appendChild(div);
+for (let i = 0; i < 16; i++){
+    let row = document.createElement('div');
+    container.appendChild(row);
+    for (let j = 0; j < 16; j++){
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        row.appendChild(cell);
     }
 }
+
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', mouseOver);
+})
+
+
+function mouseOver() {
+    cells.style.color = "red";
+  }
+  
